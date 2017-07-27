a849f46fb2d5ee2869a262f777eee2e6d2effc65
GabrielMayerSE <30503997+GabrielMayerSE@users.noreply.github.com>
2017-07-27 13:45:51 -0300

    Add files via upload

---
+++ ControllerHelloWordMSG.java
@@ -0,0 +1,14 @@
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by bielm on 27/07/2017.
+ */
+public class ControllerHelloWordMSG implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
---
+++ HelloWordMSG.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by bielm on 27/07/2017.
+ */
+public class HelloWordMSG extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent parent = FXMLLoader.load(getClass().getResource("hellowordmsg.fxml"));
+        Scene scene = new Scene(parent);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Hello, Word Application");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
